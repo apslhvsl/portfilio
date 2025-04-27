c067eb263aa01aca5777c3394c334b34d6da509d
apslhvsl <apslhvsl@gmail.com>
2025-04-27 18:49:52 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Akshay Pratap Singh</h3>
+          <h3>Designed and Developed by Akshay</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3></h3>
